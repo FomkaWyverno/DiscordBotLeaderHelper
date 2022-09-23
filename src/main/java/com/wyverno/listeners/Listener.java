f9d39043371986839a5bf86a2ef858fe75d649ae
Fomka_Wyverno <kostiaklimishagghh@gmail.com>
2022-09-23 16:35:22 +0300

    Add ignored category

--- src/main/java/com/wyverno/listeners/Listener.java
+++ src/main/java/com/wyverno/listeners/Listener.java
@@ -11,15 +11,18 @@ import org.slf4j.LoggerFactory;
 public class Listener extends ListenerAdapter {
 
     private static final Logger logger = LoggerFactory.getLogger(Listener.class);
+    private static final long LOG_CATEGORY = 1022453081654444072L;
 
     @Override
     public void onMessageReceived(@NotNull MessageReceivedEvent event) {
-        logger.info("Got message");
-        logger.info("Message: " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
-        if (event.getChannel().getIdLong() == Main.CHAT_REQUEST_ROLE) {
-            logger.info("Got new request Role");
-            RequestRole.requestRoleComplete(event.getMessage(),event.getMember());
-            logger.info("Finish complete request role");
+        if (event.getMessage().getCategory() == null || event.getMessage().getCategory().getIdLong() != LOG_CATEGORY) {
+            logger.info("Got message");
+            logger.info("Message: " + event.getAuthor().getName() + ": " + event.getMessage().getContentDisplay());
+            if (event.getChannel().getIdLong() == Main.CHAT_REQUEST_ROLE) {
+                logger.info("Got new request Role");
+                RequestRole.requestRoleComplete(event.getMessage(),event.getMember());
+                logger.info("Finish complete request role");
+            }
         }
     }
 }
